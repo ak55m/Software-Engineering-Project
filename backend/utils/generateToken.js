013584b0a8fcfe52d711a7595a0f301f9f00dde8
ak55m <akeemoshione9@yahoo.com>
2023-11-23 16:49:06 -0600

    project

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,1 +1,9 @@
-hi
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+export default generateToken;
