b32a824fed87e674118e993aae99d700a820f683
ak55m <akeemoshione9@yahoo.com>
2023-11-23 16:45:21 -0600

    project

---
+++ backend/data/lotterytickets.js
@@ -0,0 +1,36 @@
+const products = [
+    {
+      name: "Velcro Ballerinas For Girls  (Pink)",
+      seller: "Red raider",
+      image: "/images/6.png",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      price: 89,
+      countInStock: 3,
+      rating: 4,
+      numReviews: 4,
+      address: "2500 E Broadway St, Lubbock, TX, United States",
+      location: {type: "Point", 
+      coordinates: [-96.96172, 33.01366]
+      },
+      
+    },
+    {
+      name: "Women Red Heels Sandal",
+      seller: "Red raider cool",
+      image: "/images/1.png",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      price: 29,
+      countInStock: 20,
+      rating: 0,
+      numReviews: 0,
+      address: "2500 E Broadway St, Lubbock, TX, United States",
+      location: {type: "Point", 
+      coordinates: [-101.81206512451172, 33.5850715637207]
+      },
+    },
+  ];
+  
+  export default products;
+  
--- backend/data/users.js
+++ backend/data/users.js
@@ -1,1 +1,46 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Akeem Mohammed",
+    email: "akeem@lottery.com",
+    password: bcrypt.hashSync("projectlottery", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Chihiro Tashiro",
+    email: "chihiro@lottery.com",
+    password: bcrypt.hashSync("projectlotery", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Kimora Sananikone",
+    email: "kimora@lottery.com",
+    password: bcrypt.hashSync("projectlottery", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Sebastian Morones",
+    email: "sebastian@lottery.com",
+    password: bcrypt.hashSync("projectlottery", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Lottery Buyer1",
+    email: "buyer1@gmail.com",
+    password: bcrypt.hashSync("123456789", 10),
+  },
+  {
+    name: "Lottery Buyer2",
+    email: "buyer2@gmail.com",
+    password: bcrypt.hashSync("123456789", 10),
+  },
+  {
+    name: "Lottery Buyer3",
+    email: "buyer3@gmail.com",
+    password: bcrypt.hashSync("123456789", 10),
+  },
+];
+
+export default users;
 
