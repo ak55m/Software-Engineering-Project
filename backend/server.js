995a244400f6b42d563e27c29edeb1b63d110e53
ak55m <akeemoshione9@yahoo.com>
2023-11-25 11:41:26 -0600

    project

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,6 @@ app.post("/create-payment", async(req, res)=> {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, console.log(`server run in port ${PORT}`));
